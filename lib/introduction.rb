aedb31f2b798d5c7423c42dca6678c1017cd12d8
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-12 08:24:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,8 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+
+#
 def introduction(name)
 
 end
@@ -14,6 +16,11 @@ end
 
 
 
+
+def introduction_with_language_optional(name, language="Ruby")
+
+
+end
 
 
 
